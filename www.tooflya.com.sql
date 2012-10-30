0237586e5f1b45ae1c8008f9c2b5755e7358c75f
Igor Mats <igor.mats@yandex.ru>
2012-10-31 00:09:10 +0200

    Remove border from the apps icons

--- www.tooflya.com.sql
+++ www.tooflya.com.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.63, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.5.27, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: www.tooflya.com
 -- ------------------------------------------------------
--- Server version	5.1.63-0ubuntu0.11.10.1
+-- Server version	5.5.27-0ubuntu2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -180,7 +180,7 @@ CREATE TABLE `tweets_update` (
 
 LOCK TABLES `tweets_update` WRITE;
 /*!40000 ALTER TABLE `tweets_update` DISABLE KEYS */;
-INSERT INTO `tweets_update` VALUES ('2012-10-30 20:37:00');
+INSERT INTO `tweets_update` VALUES ('2012-10-30 22:04:56');
 /*!40000 ALTER TABLE `tweets_update` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -222,4 +222,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-10-31  1:58:59
+-- Dump completed on 2012-10-31  0:09:10
