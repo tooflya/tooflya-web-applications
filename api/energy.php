9ce1028623f65e8dda8baec270ac7bae3dc85b42
Igor Mats <igor.mats@yandex.ru>
2014-08-29 12:18:14 +0300

    Update.

--- api/energy.php
+++ api/energy.php
@@ -87,10 +87,10 @@ namespace API
      */
     public function find()
     {print 123123;
-      $this->response('energy', array(
+      /*$this->response('energy', array(
         'restore' => $this->queries('energy.find'),
         'secret' => $this->secret
-      ));
+      ));*/
     }
   }
 }
