dea6685cde8c13555cd838bb976dd98389dfb710
Igor Mats <igor.mats@yandex.ru>
2014-08-29 12:22:00 +0300

    Update.

--- api/component.php
+++ api/component.php
@@ -404,10 +404,10 @@ namespace API
           // TODO: Send notification about this present.
         }
         break;
-        case 'energy.find':print 1;
+        case 'energy.find':
         $data = array();
 
-        $query1 = mysql_query("SELECT `users`.*, `energy`.`time` FROM `users`, `energy` WHERE `application` = '$this->application' AND `energy`.`uid2` = '$this->uid' AND `energy`.`received` = '0' AND `users`.`uid` = `energy`.`uid1`");
+        $query1 = mysql_query("SELECT `users`.*, `energy`.`time` FROM `users`, `energy` WHERE `energy`.`application` = '$this->application' AND `energy`.`uid2` = '$this->uid' AND `energy`.`received` = '0' AND `users`.`uid` = `energy`.`uid1`");
         //$query2 = mysql_query("UPDATE `energy` WHERE `application` = '$this->application' AND `uid2` = '$this->uid' SET `received` = '1'");
 
         while(false !== ($result = mysql_fetch_assoc($query1)))
